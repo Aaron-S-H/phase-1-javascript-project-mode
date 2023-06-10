e77fcebeb1326e776bc05d17becc2de483381f45
Aaron Hemphill <hemphill.aaron@gmail.com>
2023-06-10 15:21:13 +0200

    added js, event listener

---
+++ phase-1-assessment-project/index.js
@@ -0,0 +1,4 @@
+const form = document.getElementById("form");
+const addWine = document.querySelector(".addWine");
+const addWineForm = document.querySelector("addWineForm")
+addWine.addEventListener("mouseover",(e)=> console.log(e.target));
