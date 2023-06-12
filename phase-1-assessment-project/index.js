da55b492b02f34ea7f03cff6e6984839f2e4e689
Aaron Hemphill <hemphill.aaron@gmail.com>
2023-06-12 15:47:11 +0200

    js and css features, hidden submit form and hover pseudoclass

--- phase-1-assessment-project/index.js
+++ phase-1-assessment-project/index.js
@@ -1,4 +1,7 @@
+
+const input = document.querySelector("input");
 const form = document.getElementById("form");
 const addWine = document.querySelector(".addWine");
-const addWineForm = document.querySelector("addWineForm")
-addWine.addEventListener("mouseover",(e)=> console.log(e.target));
+const addWineForm = document.querySelector(".addWineForm");
+addWine.addEventListener("click",()=>  { if(form.className === "hidden"){form.className = "addWineForm"; }
+else{form.className = "hidden";}});
