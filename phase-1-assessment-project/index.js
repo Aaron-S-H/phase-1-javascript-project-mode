a89470d440918013e5173276860820184c79aaad
Aaron Hemphill <hemphill.aaron@gmail.com>
2023-06-14 11:59:26 +0200

    json server corrections

--- phase-1-assessment-project/index.js
+++ phase-1-assessment-project/index.js
@@ -1,3 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+    getAllWines();
+
+
 
 const input = document.querySelector("input");
 const form = document.getElementById("form");
@@ -5,3 +9,35 @@ const addWine = document.querySelector(".addWine");
 const addWineForm = document.querySelector(".addWineForm");
 addWine.addEventListener("click",()=>  { if(form.className === "hidden"){form.className = "addWineForm"; }
 else{form.className = "hidden";}});
+
+
+function getAllWines() {
+    fetch("http://localhost:3000/wines")
+    .then((res) => res.json())
+    .then((wineData) => wineData.forEach((wine) => wineHandler(wine)));
+}
+
+function wineHandler(wine) {
+    const collection = document.querySelector("#wineList");
+    let card = document.createElement("li");
+    card.className = "card";
+    card.innerHTML = `
+    <h2>${wine.name}</h2>
+    <img src="${wine.frontImageURL}" class="toy-avatar"/>
+    <p class="
+    <p class="ratings"> ${wine.averageUserRating} average user rating</p>
+    <select name="userRatings" id="ratings>
+    <option value="5">5</option>
+    <option value="4">4</option>
+    <option value="3">3</option>
+    <option value="2">2</option>
+    <option value="1">1</option>
+    <option value="0">0</option>
+    </select>
+    `;
+}
+collection.appendChild(card);
+
+
+
+});
