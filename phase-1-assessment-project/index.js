b1719620ec42eb7be48a66893c8875a6b46d04dd
Aaron Hemphill <hemphill.aaron@gmail.com>
2023-06-15 11:55:08 +0200

    mouseover functional

--- phase-1-assessment-project/index.js
+++ phase-1-assessment-project/index.js
@@ -20,25 +20,56 @@ function getAllWines() {
 function wineHandler(wine) {
     const collection = document.querySelector("ul");
     let card = document.createElement("li");
+    let h2 = document.querySelector("h2");
     card.className = "card";
-    card.innerHTML = `
-    <h2>${wine.name}</h2>
-    <img src="${wine.frontImageURL}" class="toy-avatar"/>
-    <p class="
-    <p class="ratings"> ${wine.averageUserRating} average user rating</p>
-    <select name="userRatings" id="ratings>
+    let cardFront = `
+    <h2 class = "cardFront">${wine.name}</h2>
+    <h3>Origin: ${wine.origin}<br>
+       ${wine.year}</h3>
+    <img src="${wine.frontImageURL}" class="wine-photo"/>
+    <br>
+    <select name="userRatings" class="ratings">
+    <option value="">Rate This Wine</option>
     <option value="5">5</option>
     <option value="4">4</option>
     <option value="3">3</option>
     <option value="2">2</option>
     <option value="1">1</option>
-    <option value="0">0</option>
     </select>
-    `;
+    <br>
+    <p class="ratings">average user rating: ${wine.averageUserRating}</p>
+    `
+    ;
+    let cardBack = `
+    <h2 class = "cardBack">${wine.name}</h2>
+    <h3>Origin: ${wine.alcohol}<br>
+       ${wine.price}</h3>
+    <img src="${wine.backImageURL}" class="wine-photo"/>
+    <br>
+    <select name="userRatings" class="ratings">
+    <option value="">Rate This Wine</option>
+    <option value="5">5</option>
+    <option value="4">4</option>
+    <option value="3">3</option>
+    <option value="2">2</option>
+    <option value="1">1</option>
+    </select>
+    <br>
+    <p class="ratings">average user rating: ${wine.averageUserRating}</p>
+    `
+    ;
+
+    card.innerHTML = cardFront;
+
+    let img = card.querySelector(".wine-photo");
+    img.addEventListener("mouseover",() => { if(wine.year){card.innerHTML = cardBack;}
+    else{card.innerHTML = cardFront;}
+    });
+   
+
     collection.appendChild(card);
 
 }
-
 
 
 
